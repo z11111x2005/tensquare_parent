8505b5c0a077401f2501c2dc1fcb04201c27e54d
z11111x2005 <2417788343@qq.com>
2019-02-21 00:34:51 +0800

    修改部分代码格式异常

--- tensquare_user/src/main/java/com/tensquare/user/UserApplication.java
+++ tensquare_user/src/main/java/com/tensquare/user/UserApplication.java
@@ -1,24 +1,26 @@
 package com.tensquare.user;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import util.IdWorker;
+
 @SpringBootApplication
 public class UserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
 
-	@Bean
-	public IdWorker idWorkker(){
-		return new IdWorker(1, 1);
-	}
+    @Bean
+    public IdWorker idWorkker() {
+        return new IdWorker(1, 1);
+    }
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-	
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
