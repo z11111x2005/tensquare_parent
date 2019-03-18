8f681fe51703fe06306cf63664bc0756e77df8fe
liuym <2417788343@qq.com>
2019-03-18 18:26:48 +0800

    第8章:自动更新配置(需要在controller中加入注解@RefreshScope)

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -6,6 +6,7 @@ import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,49 +15,50 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping("/laber")
+@RefreshScope
 public class LabelController {
 
     @Autowired
     private LabelService labelService;
 
     @GetMapping()
-    public Result findAll(){
+    public Result findAll() {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
 
     @GetMapping("/{labelId}")
-    public Result findById(@PathVariable("labelId") String labelId){
+    public Result findById(@PathVariable("labelId") String labelId) {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findById(labelId));
     }
 
     @PostMapping()
-    public Result save(@RequestBody Label label){
+    public Result save(@RequestBody Label label) {
         labelService.save(label);
         return new Result(true, StatusCode.OK, "添加成功");
     }
 
     @PutMapping()
-    public Result update(@PathVariable String labelId, @RequestBody Label label){
+    public Result update(@PathVariable String labelId, @RequestBody Label label) {
         label.setId(labelId);
         labelService.update(label);
         return new Result(true, StatusCode.OK, "更新成功");
     }
 
     @DeleteMapping()
-    public Result delete(@PathVariable String labelId){
+    public Result delete(@PathVariable String labelId) {
         labelService.deleteById(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
     }
 
     @PostMapping("/search")
-    public Result findSearch(@RequestBody Label label){
+    public Result findSearch(@RequestBody Label label) {
         List<Label> list = labelService.findSearch(label);
         System.out.println(list);
         return new Result(true, StatusCode.OK, "查询成功", list);
     }
 
     @PostMapping("/search/{page}/{size}")
-    public Result pageQuery(@PathVariable("page") int page, @PathVariable("size") int size, @RequestBody Label label){
+    public Result pageQuery(@PathVariable("page") int page, @PathVariable("size") int size, @RequestBody Label label) {
         Page<Label> pageDate = labelService.pageQuery(label, size, page);
         return new Result(true, StatusCode.OK, "查询成功", new PageResult<Label>(pageDate.getTotalElements(), pageDate.getContent()));
     }
